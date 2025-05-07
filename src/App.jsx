c6a3e39683400014292d22703414bf1985db9659
Michael Dessen <michael@mdessen2024.local>
2025-05-06 22:57:54 -0700

    Auto-generated commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import OpenGrid from './OpenGrid';
+
+function App() {
+  return (
+    <div className="App">
+      <OpenGrid />
+    </div>
+  );
+}
+
+export default App;
