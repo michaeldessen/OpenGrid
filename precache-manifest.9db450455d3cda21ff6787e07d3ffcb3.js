f83d8489923999c78a30fda6892135cf62ee439c
Michael Dessen <michael@mdessen2024.local>
2025-05-06 23:53:02 -0700

    Auto-generated commit

---
+++ precache-manifest.9db450455d3cda21ff6787e07d3ffcb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9820b1bf274a4b5de132bbbbda58aba6",
+    "url": "/OpenGrid/index.html"
+  },
+  {
+    "revision": "19f2527a889a5bf57390",
+    "url": "/OpenGrid/static/css/main.e9f15a12.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/OpenGrid/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/OpenGrid/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "19f2527a889a5bf57390",
+    "url": "/OpenGrid/static/js/main.6390db9c.chunk.js"
+  },
+  {
+    "revision": "ea0246b7a0a8a6269c61",
+    "url": "/OpenGrid/static/js/runtime~main.e66c94fc.js"
+  }
+]);
